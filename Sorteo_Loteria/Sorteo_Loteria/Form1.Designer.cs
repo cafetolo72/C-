c67b760ee963d2be49f310d23a2e416f6a59982c
JOB-20-21 <osorio21205@iesmarenostrum.com>
2021-03-03 13:51:20 +0100

    Versión inicial

--- Sorteo_Loteria/Sorteo_Loteria/Form1.Designer.cs
+++ Sorteo_Loteria/Sorteo_Loteria/Form1.Designer.cs
@@ -29,13 +29,70 @@ namespace Sorteo_Loteria
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.bIntroducirN = new System.Windows.Forms.Button();
+            this.bComenzarS = new System.Windows.Forms.Button();
+            this.bMostrarR = new System.Windows.Forms.Button();
+            this.bComparar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bIntroducirN
+            // 
+            this.bIntroducirN.Location = new System.Drawing.Point(141, 55);
+            this.bIntroducirN.Name = "bIntroducirN";
+            this.bIntroducirN.Size = new System.Drawing.Size(125, 23);
+            this.bIntroducirN.TabIndex = 0;
+            this.bIntroducirN.Text = "Introducir Números";
+            this.bIntroducirN.UseVisualStyleBackColor = true;
+            this.bIntroducirN.Click += new System.EventHandler(this.bIntroducirN_Click);
+            // 
+            // bComenzarS
+            // 
+            this.bComenzarS.Location = new System.Drawing.Point(141, 116);
+            this.bComenzarS.Name = "bComenzarS";
+            this.bComenzarS.Size = new System.Drawing.Size(125, 23);
+            this.bComenzarS.TabIndex = 1;
+            this.bComenzarS.Text = "Comenzar Sorteo";
+            this.bComenzarS.UseVisualStyleBackColor = true;
+            // 
+            // bMostrarR
+            // 
+            this.bMostrarR.Location = new System.Drawing.Point(141, 168);
+            this.bMostrarR.Name = "bMostrarR";
+            this.bMostrarR.Size = new System.Drawing.Size(125, 23);
+            this.bMostrarR.TabIndex = 2;
+            this.bMostrarR.Text = "Mostrar Números";
+            this.bMostrarR.UseVisualStyleBackColor = true;
+            // 
+            // bComparar
+            // 
+            this.bComparar.Location = new System.Drawing.Point(141, 232);
+            this.bComparar.Name = "bComparar";
+            this.bComparar.Size = new System.Drawing.Size(125, 23);
+            this.bComparar.TabIndex = 3;
+            this.bComparar.Text = "Comparar Resultado";
+            this.bComparar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bComparar);
+            this.Controls.Add(this.bMostrarR);
+            this.Controls.Add(this.bComenzarS);
+            this.Controls.Add(this.bIntroducirN);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button bIntroducirN;
+        private System.Windows.Forms.Button bComenzarS;
+        private System.Windows.Forms.Button bMostrarR;
+        private System.Windows.Forms.Button bComparar;
     }
 }
 
--- Sorteo_Loteria/Sorteo_Loteria/Form1.cs
+++ Sorteo_Loteria/Sorteo_Loteria/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Collections;
 
 namespace Sorteo_Loteria
 {
@@ -15,6 +17,101 @@ namespace Sorteo_Loteria
         public Form1()
         {
             InitializeComponent();
+        }
+        ArrayList lista1 = new ArrayList();
+        ArrayList lista2 = new ArrayList();
+
+        void IntroducirNumero(ArrayList lista)
+        {
+            int num = 1;
+            int i = 1;
+            while (i <= 6)
+            {
+                num = int.Parse(Interaction.InputBox("Introduce un número entre el 1 y 49 :  " + i + " de 6"));
+                if (num >= 1 && num <= 49)
+                {
+                    if (!(lista.Contains(num)))
+                    {
+                        lista.Add(num);
+                        i++;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Introduce un número no repetido");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Debes introducir un número entre el 1 y 49");
+                }
+            }
+        }
+        void RealizarSorteo(ArrayList lista2)
+        {
+            int cont, num;
+            Random r = new Random();
+            cont = 0;
+            while (cont < 6)
+            {
+                num = r.Next(1, 50);
+                if (!(lista2.Contains(num)))
+                {
+                    lista2.Add(num);
+                    cont++;
+                }
+            }
+        }
+        string Aciertos(ArrayList lista1, ArrayList lista2)
+        {
+            string texto = "";
+            int aciertos = 0;
+            foreach (int num in lista2)
+            {
+                if (lista1.Contains(num))
+                {
+                    aciertos++;
+                    texto += num + " ,";
+                }
+            }
+            if (aciertos >= 1)
+                texto = "Has acertado " + aciertos + "números : " + texto;
+            else
+                texto = "Lo siento no ha habido aciertos";
+            return texto;
+        }
+        string MostrarLista(ArrayList lista1)
+        {
+            string texto = "";
+            foreach (int num in lista1)
+            {
+                texto += num + ",";
+            }
+            return texto;
+        }
+        string MostrarListas(ArrayList lista1, ArrayList lista2)
+        {
+            string texto = "Números del usurio : " + MostrarLista(lista1) + "\n";
+            texto += "Números de la máquina : " + MostrarLista(lista2);
+            return texto;
+        }
+        private void bIntroducirN_Click(object sender, EventArgs e)
+        {
+            IntroducirNumero(lista1);
+        }
+
+        private void bMostrarR_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(MostrarListas(lista1, lista2));
+        }
+
+        private void bComparar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Aciertos(lista1, lista2));
+        }
+
+        private void bComenzarS_Click(object sender, EventArgs e)
+        {
+            RealizarSorteo(lista2);
         }
     }
 }
