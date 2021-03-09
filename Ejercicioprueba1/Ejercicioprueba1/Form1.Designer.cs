a9698f33bed4106c81a008a9568af1576825a982
JOB-20-21 <osorio21205@iesmarenostrum.com>
2021-03-09 19:22:29 +0100

    Máximo común divisor

---
+++ Ejercicioprueba1/Ejercicioprueba1/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace Ejercicioprueba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bCalcular = new System.Windows.Forms.Button();
+            this.tNum1 = new System.Windows.Forms.TextBox();
+            this.tNum2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bCalcular
+            // 
+            this.bCalcular.Location = new System.Drawing.Point(151, 114);
+            this.bCalcular.Name = "bCalcular";
+            this.bCalcular.Size = new System.Drawing.Size(75, 23);
+            this.bCalcular.TabIndex = 0;
+            this.bCalcular.Text = "Calcular";
+            this.bCalcular.UseVisualStyleBackColor = true;
+            this.bCalcular.Click += new System.EventHandler(this.bCalcular_Click);
+            // 
+            // tNum1
+            // 
+            this.tNum1.Location = new System.Drawing.Point(129, 56);
+            this.tNum1.Name = "tNum1";
+            this.tNum1.Size = new System.Drawing.Size(100, 20);
+            this.tNum1.TabIndex = 1;
+            // 
+            // tNum2
+            // 
+            this.tNum2.Location = new System.Drawing.Point(272, 55);
+            this.tNum2.Name = "tNum2";
+            this.tNum2.Size = new System.Drawing.Size(100, 20);
+            this.tNum2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tNum2);
+            this.Controls.Add(this.tNum1);
+            this.Controls.Add(this.bCalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bCalcular;
+        private System.Windows.Forms.TextBox tNum1;
+        private System.Windows.Forms.TextBox tNum2;
+    }
+}
+
---
+++ Ejercicioprueba1/Ejercicioprueba1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicioprueba1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int EncontrarMenor(int num1, int num2)
+        {
+            int menor = 0;
+
+            if (num1 < num2)
+                menor = num1;
+            else
+                menor = num2;
+
+            return menor;
+        }
+
+        int MaximoComunDivisor(int num1, int num2)
+        {
+            int mcd = 1, menor;
+            menor = EncontrarMenor(num1, num2);
+            for (int i = 0; i < menor; i++)
+            {
+                if(num1%i==0 && num2 % i == 0)
+                {
+                    mcd = i;
+                }
+            }
+            return mcd;
+        }
+
+        private void bCalcular_Click(object sender, EventArgs e)
+        {
+            int num1, num2;
+            num1 = int.Parse(tNum1.Text);
+            num2 = int.Parse(tNum2.Text);
+            MessageBox.Show("El máximo común divisor es : " + EncontrarMenor(num1, num2));
+        }
+    }
+}
