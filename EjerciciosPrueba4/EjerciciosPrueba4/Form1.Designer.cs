80491a33c601e563e09f6ad9bea9416ff93162e0
JOB-20-21 <osorio21205@iesmarenostrum.com>
2021-03-13 09:40:42 +0100

    Ejercicio que cambia los valores repetidos a -1

---
+++ EjerciciosPrueba4/EjerciciosPrueba4/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace EjerciciosPrueba4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bLeerVector = new System.Windows.Forms.Button();
+            this.bCambiar = new System.Windows.Forms.Button();
+            this.bMostrarVector = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bLeerVector
+            // 
+            this.bLeerVector.Location = new System.Drawing.Point(136, 62);
+            this.bLeerVector.Name = "bLeerVector";
+            this.bLeerVector.Size = new System.Drawing.Size(118, 23);
+            this.bLeerVector.TabIndex = 0;
+            this.bLeerVector.Text = "Leer Vector ";
+            this.bLeerVector.UseVisualStyleBackColor = true;
+            this.bLeerVector.Click += new System.EventHandler(this.bLeerVector_Click);
+            // 
+            // bCambiar
+            // 
+            this.bCambiar.Location = new System.Drawing.Point(136, 119);
+            this.bCambiar.Name = "bCambiar";
+            this.bCambiar.Size = new System.Drawing.Size(118, 23);
+            this.bCambiar.TabIndex = 1;
+            this.bCambiar.Text = "Sustituir repetidos";
+            this.bCambiar.UseVisualStyleBackColor = true;
+            this.bCambiar.Click += new System.EventHandler(this.bCambiar_Click);
+            // 
+            // bMostrarVector
+            // 
+            this.bMostrarVector.Location = new System.Drawing.Point(136, 190);
+            this.bMostrarVector.Name = "bMostrarVector";
+            this.bMostrarVector.Size = new System.Drawing.Size(118, 23);
+            this.bMostrarVector.TabIndex = 2;
+            this.bMostrarVector.Text = "Mostrar Vector";
+            this.bMostrarVector.UseVisualStyleBackColor = true;
+            this.bMostrarVector.Click += new System.EventHandler(this.bMostrarVector_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bMostrarVector);
+            this.Controls.Add(this.bCambiar);
+            this.Controls.Add(this.bLeerVector);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bLeerVector;
+        private System.Windows.Forms.Button bCambiar;
+        private System.Windows.Forms.Button bMostrarVector;
+    }
+}
+
---
+++ EjerciciosPrueba4/EjerciciosPrueba4/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace EjerciciosPrueba4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[] vector = new int[10];
+
+        void LeerVector(int [] vector)
+        {
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] = int.Parse(Interaction.InputBox("Introduce números : " + i));
+            }
+        }
+
+        void CambioVector(ref int [] vector)
+        {
+            int i,j;
+            int cont = 0;
+            for ( i = 0; i < vector.Length-1; i++)
+            {
+                if(vector[i]!= -1)
+                    for (j = i+1; j < vector.Length; j++)
+                    {
+                        if (vector[j] == vector[i])
+                        {
+                            vector[j] = -1;
+                            cont++;
+                        }
+                    }
+            }
+        }
+        string MostrarVector(int [] vector)
+        {
+            string texto = "";
+            for (int i = 0; i < vector.Length; i++)
+            {
+                texto += vector[i]+ ",";
+            }
+            return texto;
+        }
+        private void bLeerVector_Click(object sender, EventArgs e)
+        {
+            LeerVector(vector);
+        }
+
+        private void bCambiar_Click(object sender, EventArgs e)
+        {
+            CambioVector(ref vector);
+        }
+
+        private void bMostrarVector_Click(object sender, EventArgs e)
+        {
+            string texto= MostrarVector(vector);
+            MessageBox.Show(texto);
+        
+        }
+    }
+}
