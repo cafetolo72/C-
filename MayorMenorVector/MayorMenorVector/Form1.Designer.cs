7f5a87cb60ae3d523992fb138fea9bf92f5c4a75
JOB-20-21 <osorio21205@iesmarenostrum.com>
2021-03-14 11:48:57 +0100

    Programa que lee 20 numeros y devuelve mayor , menor y las veces que se repite

---
+++ MayorMenorVector/MayorMenorVector/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace MayorMenorVector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bLeerVector = new System.Windows.Forms.Button();
+            this.bMostrarMenor = new System.Windows.Forms.Button();
+            this.bMostrarMayor = new System.Windows.Forms.Button();
+            this.bMostrarVector = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bLeerVector
+            // 
+            this.bLeerVector.Location = new System.Drawing.Point(113, 70);
+            this.bLeerVector.Name = "bLeerVector";
+            this.bLeerVector.Size = new System.Drawing.Size(123, 23);
+            this.bLeerVector.TabIndex = 0;
+            this.bLeerVector.Text = "Leer Vector ";
+            this.bLeerVector.UseVisualStyleBackColor = true;
+            this.bLeerVector.Click += new System.EventHandler(this.bLeerVector_Click);
+            // 
+            // bMostrarMenor
+            // 
+            this.bMostrarMenor.Location = new System.Drawing.Point(113, 140);
+            this.bMostrarMenor.Name = "bMostrarMenor";
+            this.bMostrarMenor.Size = new System.Drawing.Size(123, 23);
+            this.bMostrarMenor.TabIndex = 1;
+            this.bMostrarMenor.Text = "Mostrar Menor";
+            this.bMostrarMenor.UseVisualStyleBackColor = true;
+            this.bMostrarMenor.Click += new System.EventHandler(this.bMostrarMenor_Click);
+            // 
+            // bMostrarMayor
+            // 
+            this.bMostrarMayor.Location = new System.Drawing.Point(115, 208);
+            this.bMostrarMayor.Name = "bMostrarMayor";
+            this.bMostrarMayor.Size = new System.Drawing.Size(121, 23);
+            this.bMostrarMayor.TabIndex = 2;
+            this.bMostrarMayor.Text = "Mostrar Mayor";
+            this.bMostrarMayor.UseVisualStyleBackColor = true;
+            this.bMostrarMayor.Click += new System.EventHandler(this.bMostrarMayor_Click);
+            // 
+            // bMostrarVector
+            // 
+            this.bMostrarVector.Location = new System.Drawing.Point(110, 279);
+            this.bMostrarVector.Name = "bMostrarVector";
+            this.bMostrarVector.Size = new System.Drawing.Size(126, 23);
+            this.bMostrarVector.TabIndex = 3;
+            this.bMostrarVector.Text = "Mostrar Vector";
+            this.bMostrarVector.UseVisualStyleBackColor = true;
+            this.bMostrarVector.Click += new System.EventHandler(this.bMostrarVector_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bMostrarVector);
+            this.Controls.Add(this.bMostrarMayor);
+            this.Controls.Add(this.bMostrarMenor);
+            this.Controls.Add(this.bLeerVector);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bLeerVector;
+        private System.Windows.Forms.Button bMostrarMenor;
+        private System.Windows.Forms.Button bMostrarMayor;
+        private System.Windows.Forms.Button bMostrarVector;
+    }
+}
+
---
+++ MayorMenorVector/MayorMenorVector/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+namespace MayorMenorVector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        const int knum = 20;
+        int[] vector = new int[knum];
+
+        void LeerVector(int [] vector)
+        {
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] = int.Parse(Interaction.InputBox("Introduce número : " + i));
+            }
+        }
+
+        int Mayor(int[] vector)
+        {
+           int mayor = vector[0];
+            for (int i = 0; i < vector.Length; i++)
+            {
+                if(vector[i] >= mayor)
+                {
+                    mayor = vector[i];
+                }
+            }
+            return mayor;
+        }
+        int Menor(int [] vector)
+        {
+            int menor = vector[0];
+            for (int i = 0; i < vector.Length; i++)
+            {
+                if(vector[i]<=menor)
+                {
+                    menor = vector[i];
+                }
+            }
+            return menor;
+        }
+        void ContarVecesMenor(int [] vector,int menor, out int vecesmenor)
+        {
+            vecesmenor = 0;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                if (vector[i] == menor)
+                    vecesmenor ++;
+            }
+        }
+        void ContarVecesMayor(int[]vector, int mayor, out int vecesmayor)
+        {
+            vecesmayor = 0;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                if (vector[i] == mayor)
+                    vecesmayor++;
+            }
+        }
+
+        string MostrarVector(int [] vector)
+        {
+            string texto = "";
+            for (int i = 0; i < vector.Length; i++)
+            {
+                texto += vector[i] + ",";
+            }
+            return texto;
+        }
+
+        private void bLeerVector_Click(object sender, EventArgs e)
+        {
+            LeerVector(vector);
+        }
+
+        private void bMostrarMenor_Click(object sender, EventArgs e)
+        {
+            int vecesmenor = 0;
+            int menor = Menor(vector);
+            ContarVecesMenor(vector, menor, out vecesmenor);
+            MessageBox.Show("El número menor es : " + menor + "\n" + "Y se repite "+ vecesmenor + " veces");
+        }
+
+        private void bMostrarMayor_Click(object sender, EventArgs e)
+        {
+            int vecesmayor = 0;
+            int mayor = Mayor(vector);
+            ContarVecesMayor(vector, mayor, out vecesmayor);
+            MessageBox.Show("El número mayor es : " + mayor + "\n" + "Y se repite " + vecesmayor + " veces");
+        }
+
+        private void bMostrarVector_Click(object sender, EventArgs e)
+        {
+            string texto = MostrarVector(vector);
+            MessageBox.Show(texto);
+        }
+    }
+}
