9bff4e67c0153af663981d63bbd8fcd2be24a168
JOB-20-21 <osorio21205@iesmarenostrum.com>
2021-03-14 10:56:06 +0100

    probando nueva rama

---
+++ Pasoparadelante/Pasoparadelante/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace Pasoparadelante
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bLeerVector = new System.Windows.Forms.Button();
+            this.bPasoValores = new System.Windows.Forms.Button();
+            this.bMostrarVectores = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bLeerVector
+            // 
+            this.bLeerVector.Location = new System.Drawing.Point(82, 84);
+            this.bLeerVector.Name = "bLeerVector";
+            this.bLeerVector.Size = new System.Drawing.Size(177, 23);
+            this.bLeerVector.TabIndex = 0;
+            this.bLeerVector.Text = "Leer Vector";
+            this.bLeerVector.UseVisualStyleBackColor = true;
+            this.bLeerVector.Click += new System.EventHandler(this.bLeerVector_Click);
+            // 
+            // bPasoValores
+            // 
+            this.bPasoValores.Location = new System.Drawing.Point(82, 150);
+            this.bPasoValores.Name = "bPasoValores";
+            this.bPasoValores.Size = new System.Drawing.Size(177, 23);
+            this.bPasoValores.TabIndex = 1;
+            this.bPasoValores.Text = "Pasar valores a otro vector";
+            this.bPasoValores.UseVisualStyleBackColor = true;
+            this.bPasoValores.Click += new System.EventHandler(this.bPasoValores_Click);
+            // 
+            // bMostrarVectores
+            // 
+            this.bMostrarVectores.Location = new System.Drawing.Point(82, 221);
+            this.bMostrarVectores.Name = "bMostrarVectores";
+            this.bMostrarVectores.Size = new System.Drawing.Size(177, 23);
+            this.bMostrarVectores.TabIndex = 2;
+            this.bMostrarVectores.Text = "Mostrar Vectores";
+            this.bMostrarVectores.UseVisualStyleBackColor = true;
+            this.bMostrarVectores.Click += new System.EventHandler(this.bMostrarVectores_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bMostrarVectores);
+            this.Controls.Add(this.bPasoValores);
+            this.Controls.Add(this.bLeerVector);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bLeerVector;
+        private System.Windows.Forms.Button bPasoValores;
+        private System.Windows.Forms.Button bMostrarVectores;
+    }
+}
+
---
+++ Pasoparadelante/Pasoparadelante/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Pasoparadelante
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        const int knum = 10;
+        int[] vector = new int[knum];
+        int[] vector2 = new int [knum];
+
+        void LeerVector(int [] vector)
+        {
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] = int.Parse(Interaction.InputBox("Introduce un número : " + i));
+            }
+        }
+        
+        void CambioValores(ref int [] vector, ref int [] vector2)
+        {
+            vector2[0] = vector[knum - 1];
+            int i =0;
+            int j = 1;
+            while (i < vector.Length-1)
+            {
+                vector2[j] = vector[i];
+                i++;
+                j++;
+            }
+        }
+
+        string MostrarVector(int[] vector)
+        {
+            string texto = "";
+            for (int i = 0; i < vector.Length; i++)
+            {
+                texto += vector[i] + ",";
+            }
+            return texto;
+        }
+        string MostrarVectores(int[] vector, int[] vector2)
+        {
+            string texto = "Primer Vector : ";
+            texto += MostrarVector(vector) + "\n";
+            texto += "Segundo vector : " + MostrarVector(vector2);
+            return texto;
+        }
+
+        private void bLeerVector_Click(object sender, EventArgs e)
+        {
+            LeerVector(vector);
+        }
+
+        private void bPasoValores_Click(object sender, EventArgs e)
+        {
+            CambioValores(ref vector, ref vector2);
+        }
+
+        private void bMostrarVectores_Click(object sender, EventArgs e)
+        {
+            string texto = MostrarVectores(vector, vector2);
+            MessageBox.Show(texto);
+        }
+    }
+}
