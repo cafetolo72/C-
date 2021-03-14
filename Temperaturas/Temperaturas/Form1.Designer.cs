38acb59eecb8419261994d7706d21ca586cf08b2
JOB-20-21 <osorio21205@iesmarenostrum.com>
2021-03-14 18:24:05 +0100

    Programa que lee temperaturas y saca media, mayor y menor

---
+++ Temperaturas/Temperaturas/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace Temperaturas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bIntroducir = new System.Windows.Forms.Button();
+            this.bMostrarMayor = new System.Windows.Forms.Button();
+            this.bMostrarMenor = new System.Windows.Forms.Button();
+            this.bMostrarMedia = new System.Windows.Forms.Button();
+            this.bMostrarTemperaturas = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bIntroducir
+            // 
+            this.bIntroducir.Location = new System.Drawing.Point(109, 51);
+            this.bIntroducir.Name = "bIntroducir";
+            this.bIntroducir.Size = new System.Drawing.Size(162, 23);
+            this.bIntroducir.TabIndex = 0;
+            this.bIntroducir.Text = "Introducir Temperaturas";
+            this.bIntroducir.UseVisualStyleBackColor = true;
+            this.bIntroducir.Click += new System.EventHandler(this.bIntroducir_Click);
+            // 
+            // bMostrarMayor
+            // 
+            this.bMostrarMayor.Location = new System.Drawing.Point(109, 109);
+            this.bMostrarMayor.Name = "bMostrarMayor";
+            this.bMostrarMayor.Size = new System.Drawing.Size(162, 23);
+            this.bMostrarMayor.TabIndex = 1;
+            this.bMostrarMayor.Text = "Mostrar Mayor";
+            this.bMostrarMayor.UseVisualStyleBackColor = true;
+            this.bMostrarMayor.Click += new System.EventHandler(this.bMostrarMayor_Click);
+            // 
+            // bMostrarMenor
+            // 
+            this.bMostrarMenor.Location = new System.Drawing.Point(109, 166);
+            this.bMostrarMenor.Name = "bMostrarMenor";
+            this.bMostrarMenor.Size = new System.Drawing.Size(162, 23);
+            this.bMostrarMenor.TabIndex = 2;
+            this.bMostrarMenor.Text = "Mostrar Menor";
+            this.bMostrarMenor.UseVisualStyleBackColor = true;
+            this.bMostrarMenor.Click += new System.EventHandler(this.bMostrarMenor_Click);
+            // 
+            // bMostrarMedia
+            // 
+            this.bMostrarMedia.Location = new System.Drawing.Point(109, 232);
+            this.bMostrarMedia.Name = "bMostrarMedia";
+            this.bMostrarMedia.Size = new System.Drawing.Size(162, 23);
+            this.bMostrarMedia.TabIndex = 3;
+            this.bMostrarMedia.Text = "Mostrar Media";
+            this.bMostrarMedia.UseVisualStyleBackColor = true;
+            this.bMostrarMedia.Click += new System.EventHandler(this.bMostrarMedia_Click);
+            // 
+            // bMostrarTemperaturas
+            // 
+            this.bMostrarTemperaturas.Location = new System.Drawing.Point(109, 294);
+            this.bMostrarTemperaturas.Name = "bMostrarTemperaturas";
+            this.bMostrarTemperaturas.Size = new System.Drawing.Size(162, 23);
+            this.bMostrarTemperaturas.TabIndex = 4;
+            this.bMostrarTemperaturas.Text = "Mostrar Temperaturas";
+            this.bMostrarTemperaturas.UseVisualStyleBackColor = true;
+            this.bMostrarTemperaturas.Click += new System.EventHandler(this.bMostrarTemperaturas_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bMostrarTemperaturas);
+            this.Controls.Add(this.bMostrarMedia);
+            this.Controls.Add(this.bMostrarMenor);
+            this.Controls.Add(this.bMostrarMayor);
+            this.Controls.Add(this.bIntroducir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bIntroducir;
+        private System.Windows.Forms.Button bMostrarMayor;
+        private System.Windows.Forms.Button bMostrarMenor;
+        private System.Windows.Forms.Button bMostrarMedia;
+        private System.Windows.Forms.Button bMostrarTemperaturas;
+    }
+}
+
---
+++ Temperaturas/Temperaturas/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Temperaturas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const int knum = 20;
+        int[] vector = new int[knum];
+
+        void LeerVector(int [] vector)
+        {
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] = int.Parse(Interaction.InputBox("Introducir temepratura : " + i));
+            }
+        }
+        
+        int VerMayor(int [] vector)
+        {
+            int mayor = vector[0];
+            for (int i = 0; i < vector.Length; i++)
+            {
+                if (vector[i] >= mayor)
+                    mayor = vector[i];
+            }
+            return mayor;
+        }
+
+        int VerMenor(int [] vector)
+        {
+            int menor = vector[0];
+            for (int i = 0; i < vector.Length; i++)
+            {
+                if (vector[i] <= menor)
+                    menor = vector[i];
+            }
+            return menor;
+        }
+
+        float CalcularMedia(int [] vector)
+        {
+            float media = 0;
+            float suma = 0;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                suma += vector[i];
+            }
+            media = suma / knum;
+            return media;
+        }
+        string MostrarVector(int [] vector)
+        {
+            string texto = "";
+            for (int i = 0; i < vector.Length; i++)
+            {
+                texto += vector[i] + " ,";
+            }
+            return texto;
+        }
+        private void bIntroducir_Click(object sender, EventArgs e)
+        {
+            LeerVector(vector);
+        }
+
+        private void bMostrarMayor_Click(object sender, EventArgs e)
+        {
+            int mayor=VerMayor(vector);
+            MessageBox.Show("La temperatura mayor es : " + mayor);
+        }
+
+        private void bMostrarMenor_Click(object sender, EventArgs e)
+        {
+            int menor = VerMenor(vector);
+            MessageBox.Show("La temperatura menor es : " + menor);
+        }
+
+        private void bMostrarMedia_Click(object sender, EventArgs e)
+        {
+            float media = CalcularMedia(vector);
+            MessageBox.Show("La temperatura media es : " + media);
+        }
+
+        private void bMostrarTemperaturas_Click(object sender, EventArgs e)
+        {
+            string texto = MostrarVector(vector);
+            MessageBox.Show(texto);
+        }
+    }
+}
